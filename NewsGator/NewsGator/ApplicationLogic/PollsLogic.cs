5cfbbff295b0ea3cb337271145217506e878781c
veljkozivkovic <veljko.zivk@elfak.rs>
2025-01-28 15:55:14 +0100

    CreatePoll only

--- NewsGator/NewsGator/ApplicationLogic/PollsLogic.cs
+++ NewsGator/NewsGator/ApplicationLogic/PollsLogic.cs
@@ -1,6 +1,29 @@
+using DnsClient.Internal;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using NewsGator.Dtos;
+using NewsGator.Models;
+using NewsGator.Persistence;
+
 namespace NewsGator.ApplicationLogic;
 
 public class PollsLogic
 {
+    public async Task CreatePoll(string question, List<string> options)
+    {
+        var collection = MongoSessionManager.GetCollection<Poll>("polls");
 
+        var poll = new Poll
+        {
+            Question = question,
+            Options = options.Select(option => new PollOption
+            {
+                Option = option,
+                Votes = 0
+            }).ToList(),
+            DatePosted = DateTime.Now
+        };
+
+        await collection.InsertOneAsync(poll);
+    }
 }
